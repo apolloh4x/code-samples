0895108a80696e3a2dc0a412a4400ac7b5ce28e2
apolloh4x <jessicahanakim@gmail.com>
2022-03-09 19:14:20 -0800

    adding js and css files

---
+++ JavaScript Algorithms and Data Structures/equations.js
@@ -0,0 +1,41 @@
+let myVar = 87;
+myVar++;
+
+const ourDecimal = 5.7;
+const myDecimal = 5.7;
+
+const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+let d = 11;
+let e = 9;
+let f = 3;
+
+d -= 6;
+e -= 15;
+f -= 1;
+
+let g = 5;
+let h = 12;
+let i = 4.6;
+
+g *= 5;
+h *= 3;
+i *= 10;
+
+let j= 48;
+let k = 108;
+let h = 33;
+
+j /= 12;
+k /= 4;
+h /= 11;
---
+++ JavaScript Algorithms and Data Structures/uninitialized-variables.js
@@ -0,0 +1,11 @@
+
+var b;
+var c;
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+a = 6;
+b = 15;
+c = "I am a String!";
