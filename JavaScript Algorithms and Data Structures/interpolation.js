6ee04f0a7af6a99f22a3c4a79f1497e26664b199
apolloh4x <jessicahanakim@gmail.com>
2022-03-30 17:52:35 -0700

    temperature conversion file to js

---
+++ JavaScript Algorithms and Data Structures/interpolation.js
@@ -0,0 +1,3 @@
+const myName = 'Jessica';
+const myCity = 'Venice';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
---
+++ JavaScript Algorithms and Data Structures/temperature-conversion.js
@@ -0,0 +1,14 @@
+// Weather forecast in Kelvin.
+const kelvin = 0;
+// Weather forecast in celsius.
+const celsius = kelvin - 273;
+// Weather forecast in fahrenheit.
+let fahrenheit = celsius * (9 / 5) + 32;
+// Built-in Math object to round down decimal number of Fahrenheit.
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+// Convert Celsius to Newton.
+let newton = celsius * (33 / 100);
+// Round down decimal number of Newton.
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
