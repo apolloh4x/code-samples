e916ba31d3d6b5f1419fdd8b1edbe726a88dec1a
apolloh4x <jessicahanakim@gmail.com>
2022-04-28 15:09:22 -0700

    objects files and people counter app2

--- people-counter/index.js
+++ people-counter/index.js
@@ -1,11 +1,10 @@
-// document.getElementById("count").innerText = 5
 
-
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
-let saveEl = document.getElementById("save-El");
+
 function increment(){
-    count = count + 1;
+    count += 1;
     countEl.textContent = count;  
 }
 
@@ -13,8 +12,7 @@ function save(){
     let savedCount = count + " - ";
     saveEl.textContent += savedCount;
     countEl.textContent = 0;
-    count = 0;
-    console.log(count);
+    count = 0; 
 }
 
 
